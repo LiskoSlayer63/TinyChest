d20f8edefcc195662132757189a639d6b3895065
LiskoSlayer63 <markus.ahola@hotmail.com>
2021-01-25 09:23:55 +0200

    Seriously I forgot very insignificant stuff

--- src/main/java/lizcraft/tinychest/TinyChest.java
+++ src/main/java/lizcraft/tinychest/TinyChest.java
@@ -21,8 +21,6 @@ public class TinyChest
 		
 		Logger.init(LogManager.getLogger(TinyChest.class));
 		
-		//CommonContent.init();
-		
 		CommonContent.register(modEventBus);
 		ClientContent.register(modEventBus);
 	}
